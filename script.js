546696ee585e042d503bcc47020a3354c016bd10
espinoza91592 <espinoza91592@gmail.com>
2023-10-18 12:19:01 -0500

    all

---
+++ script.js
@@ -0,0 +1,29 @@
+/*
+5+5
+10*4
+50-11
+
+*/
+
+// this is pseudocode
+
+/* 
+input the first number which is 5
+input the second number which is also 5
+add them together
+it equals 10
+
+*/
+/* 
+input the first number which is 10
+input the second number which is 4
+multiply the two
+it euals 40
+*/
+
+/*
+input the first number which is 50
+input the second number which is 11
+subtract the twon numbers
+it equals 39
+*/
